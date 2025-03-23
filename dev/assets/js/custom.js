36bff3b144932417669ac4ef84a67fb72cbc0c5d
Niko_Faccio <niko.faccio@outlook.sk>
2025-03-23 21:18:03 +0100

    modal box finish

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -11,6 +11,54 @@ const modalTrigers = document.querySelectorAll('[data-triger-modal]');
 const modalClose = document.querySelectorAll('[data-modal-close]')
 const overlay = document.querySelector('.overlay')
 const body = document.querySelector('body')
+const processVideo = document.querySelector('#video');
+const preparation = document.querySelector('#preparation')
+const cooking = document.querySelector('#cooking')
+const enjoyment = document.querySelector('#enjoyment')
+
+const play = document.querySelector('#play')
+const pause = document.querySelector('#pause')
+const stop = document.querySelector('#stop')
+
+function playVideo() {
+    processVideo.play();
+}
+
+function pauseVideo() {
+    processVideo.pause();
+}
+
+function stopVideo() {
+    processVideo.pause();
+    processVideo.currentTime = 0;
+}
+
+play.addEventListener('click', () => {
+    playVideo();
+    processVideo.volume = 0.5;
+})
+
+pause.addEventListener('click', () => {
+    pauseVideo();
+})
+
+stop.addEventListener('click', () => {
+    stopVideo();
+})
+
+
+
+function currentPreparation() {
+    processVideo.currentTime = 25;
+}
+
+function currentCooking() {
+    processVideo.currentTime = 85;
+}
+
+function currentEnjoyment() {
+    processVideo.currentTime = 158;
+}
 
 function showModal() {
     body.classList.add('overflow-hidden')
@@ -22,7 +70,24 @@ function closeModal() {
     body.classList.remove('overflow-hidden')
     overlay.classList.remove('show')
     openModal.classList.remove('show')
+    stopVideo();
 }
+
+
+preparation.addEventListener('click', () => {
+    currentPreparation();
+    processVideo.volume = 0.5;
+})
+
+cooking.addEventListener('click', () => {
+    currentCooking();
+    processVideo.volume = 0.5;
+})
+
+enjoyment.addEventListener('click', () => {
+    currentEnjoyment();
+    processVideo.volume = 0.5;
+})
 
 modalTrigers.forEach(item => {
     item.addEventListener('click', function(event) {
@@ -33,6 +98,13 @@ modalTrigers.forEach(item => {
 
         showModal()
         modal.classList.add('show')
+
+        if(modal.classList.contains('video-process')) {
+            modal.classList.add('show')
+            playVideo()
+        } else {
+            modal.classList.add('show')
+        }
     })
 })
 
@@ -41,9 +113,19 @@ modalClose.forEach(close => {
 })
 
 
-//
-//btnTrigerProcess.addEventListener('click', showModal);
-//btnModalClose.addEventListener('click', closeModal);
 
 
+
+
+const x = 3
+
+if (x < 7) {
+    console.log ('malo')
+}
+else  if (x ==7) {
+    console.log ('7')
+} 
+else {
+    console.log ('viac')
+}
 
